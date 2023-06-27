ccaae24395075d901efc81dfe262a475cb454e3a
Firemark <marpiechula@gmail.com>
2023-06-28 00:30:38 +0200

    Support bezier curves

--- art.c
+++ art.c
@@ -6,8 +6,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LINES_SIZE 1024
+#define LINES_SIZE (1024 * 3)
 #define CIRCLES_SIZE 1024
+#define BEZIER_LINES_HIGH 16
+#define BEZIER_LINES_LOW 6
 
 static struct Bitmap EXAMPLE = {{
     // clang-format off
@@ -44,6 +46,7 @@ static struct Circle *_circles;
 struct Tree {
   float main_branch_ratio;
   float side_branch_ratio;
+  float curvy_ratio;
   uint8_t top_start;
   float top_rot;
   float bottom_rot;
@@ -54,6 +57,11 @@ static inline float _random(int16_t start, int16_t end) {
 }
 
 static inline int16_t _random_int(int16_t x) { return art_random() % x; }
+
+static inline float _thickness(int16_t w) { return w * w / 2048.0 + 1.0; }
+static inline float _lerp(int16_t x0, int16_t x1, float t) {
+  return x0 * t + x1 * (1.0 - t);
+}
 
 static void _leaf(struct Point p, uint16_t size) {
   if (_circles_count >= CIRCLES_SIZE) {
@@ -67,13 +75,31 @@ static void _leaf(struct Point p, uint16_t size) {
   };
 }
 
+static struct Point _bezier(float t, struct Point b[4]) {
+  struct Point p;
+  float t_inv = 1 - t;
+  float tt = t * t;
+  float ttt = tt * t;
+  float tt_inv = t_inv * t_inv;
+  float ttt_inv = tt_inv * t_inv;
+  float t_tt_inv = t * tt_inv;
+  float tt_t_inv = tt * t_inv;
+
+  p.x = b[0].x * ttt_inv + 3 * b[1].x * t_tt_inv + 3 * b[2].x * tt_t_inv +
+        b[3].x * ttt;
+  p.y = b[0].y * ttt_inv + 3 * b[1].y * t_tt_inv + 3 * b[2].y * tt_t_inv +
+        b[3].y * ttt;
+
+  return p;
+}
+
 static void _tree(uint8_t n, struct Tree *tree, struct Point p, int16_t w,
                   float rot) {
   if (n < tree->top_start && _random_int(100) > n * 8) {
     _leaf(p, w / 20 + _random_int(5));
   }
 
-  if (n == 0 || _lines_count >= LINES_SIZE) {
+  if (n == 0 || _lines_count >= LINES_SIZE - BEZIER_LINES_HIGH) {
     return;
   }
 
@@ -81,30 +107,86 @@ static void _tree(uint8_t n, struct Tree *tree, struct Point p, int16_t w,
     return;
   }
 
-  struct Point np = {p.x + w * sin(rot), p.y - w * cos(rot)};
+  struct Point points[4];
+  points[0] = p;
+
+  float _sin = sin(rot);
+  float _cos = cos(rot);
+
+  float r1 = _random(200, 500);
+  float r2 = r1 + _random(200, 500);
+
+  float dx1 = w * r1;
+  float dy1 = tree->curvy_ratio * w * _random(-1000, +1000);
+  points[1] = (struct Point){
+      p.x + dx1 * _sin - dy1 * _cos,
+      p.y - dx1 * _cos - dy1 * _sin,
+  };
+
+  float dx2 = w * r2;
+  float dy2 = tree->curvy_ratio * w * _random(-1000, +1000);
+  points[2] = (struct Point){
+      p.x + dx2 * _sin - dy2 * _cos,
+      p.y - dx2 * _cos - dy2 * _sin,
+  };
+
+  points[3] = (struct Point){
+      p.x + w * _sin,
+      p.y - w * _cos,
+  };
+
+  struct Point old_p = p;
+
+  int16_t new_w = w * (tree->main_branch_ratio + _random(-125, 125));
+
+  if (w > 100) {
+    for (uint8_t i = 0; i < (BEZIER_LINES_HIGH - 1); i++) {
+      float t = i * (1.0 / BEZIER_LINES_HIGH);
+      struct Point new_p = _bezier(t, points);
+      _lines[_lines_count++] = (struct Line){
+          .color = _branches_color,
+          .thickness = _thickness(w),
+          .p0 = old_p,
+          .p1 = new_p,
+      };
+      old_p = new_p;
+    }
+  } else {
+    for (uint8_t i = 0; i < (BEZIER_LINES_LOW - 1); i++) {
+      float t = i * (1.0 / BEZIER_LINES_LOW);
+      struct Point new_p = _bezier(t, points);
+      float part_w = _lerp(w, new_w, t);
+      _lines[_lines_count++] = (struct Line){
+          .color = _branches_color,
+          .thickness = _thickness(part_w),
+          .p0 = old_p,
+          .p1 = new_p,
+      };
+      old_p = new_p;
+    }
+  }
 
   _lines[_lines_count++] = (struct Line){
       .color = _branches_color,
-      .thickness = w * w / 2048 + 1,
-      .p0 = p,
-      .p1 = np,
+      .thickness = _thickness(w),
+      .p0 = old_p,
+      .p1 = points[3],
   };
 
-  float dr0 = _random(200, 1000);
-  struct Point np0 = {p.x + dr0 * w * sin(rot), p.y - dr0 * w * cos(rot)};
-
-  float dr1 = _random(200, 1000);
-  struct Point np1 = {p.x + dr1 * w * sin(rot), p.y - dr1 * w * cos(rot)};
-
-  int16_t nw1 = w * (tree->main_branch_ratio + _random(-125, 125));
-  int16_t nw2 = w * (tree->side_branch_ratio + _random(-125, 125));
-  int16_t nw3 = w * (tree->side_branch_ratio + _random(-125, 125));
+  _tree(n - 1, tree, points[3], new_w, rot + _random(-125, 125));
 
   float new_rot = n > tree->top_start ? tree->top_rot : tree->bottom_rot;
 
-  _tree(n - 1, tree, np, nw1, rot + _random(-125, 125));
-  _tree(n - 1, tree, np0, nw2, rot + _random(-125, 125) + new_rot);
-  _tree(n - 1, tree, np1, nw3, rot + _random(-125, 125) - new_rot);
+  float split_right = 0.25 + _random(-100, 100);
+  struct Point side_right = _bezier(split_right, points);
+  float w_right = _lerp(w, new_w, split_right) * tree->side_branch_ratio;
+  _tree(n - 1, tree, side_right, w_right, rot + _random(-125, 125) + new_rot);
+
+  float split_left = 0.75 + _random(-100, 100);
+  struct Point side_left = _bezier(split_left, points);
+  float w_left = _lerp(w, new_w, split_left) * tree->side_branch_ratio;
+  _tree(n - 1, tree, side_left, w_left, rot + _random(-125, 125) - new_rot);
+
 }
 
 static void _grid(struct Image *image) {
@@ -170,6 +252,7 @@ static void _generate_tree(void) {
   struct Tree tree = {
       .main_branch_ratio = _random(600, 900),
       .side_branch_ratio = _random(500, 800),
+      .curvy_ratio = _random(0, 1000),
       .top_start = 2 + _random_int(2),
       .top_rot = _random(300, 900),
       .bottom_rot = _random(100, 400),
