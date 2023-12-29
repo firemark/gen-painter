ed267e1b90f191aa0659416560305a2f4b3ea8e1
Firemark <marpiechula@gmail.com>
2023-12-29 19:50:19 +0100

    Add more peaks

--- src/art/landscape.c
+++ src/art/landscape.c
@@ -1,5 +1,6 @@
 #include "art/landscape.h"
 
+#include <stdbool.h>
 #include <stdlib.h>
 
 #include "art/image/image_adv.h"
@@ -17,11 +18,17 @@ int16_t landscape_generate(void) {
 
 static void _draw_mountain(struct Image *image, int16_t x_center, int16_t width,
                            int16_t height);
+static void _draw_peak(struct Image *image, int16_t x_center, int16_t width,
+                       int16_t height, int16_t y_base_left,
+                       int16_t y_base_right, bool with_base);
+static void _draw_small_peak(struct Image *image, int16_t x_center,
+                             int16_t width, int16_t height, int16_t ratio,
+                             int16_t shift);
 static void _draw_terrain(struct Image *image);
 
 void landscape_draw(struct Image *image) {
   int16_t mountain_height = 400 + random_int(200);
-  int16_t mountain_width = 800 + random_int(400);
+  int16_t mountain_width = 1000 + random_int(400);
   int16_t x_center = IMAGE_WIDTH / 2 + random_int(100) - 200;
   if (random_int(32) > 24) {
     _draw_mountain(image, x_center + random_int(100) - 300, mountain_width,
@@ -29,10 +36,57 @@ void landscape_draw(struct Image *image) {
     _draw_mountain(image, x_center - random_int(100) + 300, mountain_width / 2,
                    mountain_height / 2);
   } else {
-    _draw_mountain(image, x_center, mountain_width,
-                   mountain_height);
+    _draw_mountain(image, x_center, mountain_width, mountain_height);
   }
   _draw_terrain(image);
+}
+
+static void _draw_mountain(struct Image *image, int16_t x_center, int16_t width,
+                           int16_t height) {
+  int16_t tiny_width = width / 8;
+  if (random_int(16) > 6) {
+    _draw_small_peak(image, x_center, width, height, 2,
+                     +tiny_width * 2 + random_int(tiny_width));
+  }
+  if (random_int(16) > 6) {
+    _draw_small_peak(image, x_center, width, height, 2,
+                     -tiny_width * 2 - random_int(tiny_width));
+  }
+  _draw_peak(image, x_center, width, height, _y, _y, false);
+  if (random_int(16) > 6) {
+    _draw_small_peak(image, x_center, width, height, 4,
+                     +tiny_width + random_int(tiny_width));
+  }
+  if (random_int(16) > 10) {
+    _draw_small_peak(image, x_center, width, height, 4,
+                     +tiny_width * 2 + random_int(tiny_width));
+  }
+  if (random_int(16) > 6) {
+    _draw_small_peak(image, x_center, width, height, 4,
+                     -tiny_width - random_int(tiny_width));
+  }
+  if (random_int(16) > 10) {
+    _draw_small_peak(image, x_center, width, height, 4,
+                     -tiny_width * 2 - random_int(tiny_width));
+  }
+}
+
+static void _draw_small_peak(struct Image *image, int16_t x_center,
+                             int16_t width, int16_t height, int16_t ratio,
+                             int16_t shift) {
+
+  int16_t w = width / ratio + random_int(width / (ratio * 2));
+  int16_t h = height / ratio + random_int(height / (ratio * 2));
+  int16_t abs_shift = abs(shift);
+  if (abs_shift + w / 2 > width / 2) {
+    w = width - 2 * abs_shift;
+  }
+  int16_t sign = shift > 0 ? 1 : -1;
+  int16_t xa = abs_shift - sign * w / 2;
+  int16_t xb = abs_shift + sign * w / 2;
+  int16_t h_left = (height * (xa - width / 2) / width) + 20 + random_int(20);
+  int16_t h_right = (height * (xb - width / 2) / width) + 20 + random_int(20);
+  _draw_peak(image, x_center + shift, w, h, _y + h_left, _y + h_right, true);
 }
 
 static void _polyfill_mountain(struct Image *image, struct Point *points,
@@ -40,55 +94,110 @@ static void _polyfill_mountain(struct Image *image, struct Point *points,
   polyfill(image, points, size, _leaves_color, threshold, _background_color);
 }
 
-static void _draw_mountain(struct Image *image, int16_t x_center, int16_t width,
-                           int16_t height) {
+static void _draw_peak(struct Image *image, int16_t x_center, int16_t width,
+                       int16_t height, int16_t y_base_left,
+                       int16_t y_base_right, bool with_base) {
   uint8_t i;
-  uint8_t peak_points_size = 12 + random_int(4);
-  uint8_t edge_points_size = 6 + random_int(4);
-  uint8_t peak_index = peak_points_size / 2 + random_int(2) - 1;
+  int16_t y_base_mid = (y_base_left + y_base_right) / 2;
+  uint8_t peak_points_size = 32 + random_int(8);
+  uint8_t edge_points_size = 12 + random_int(8);
+  uint8_t peak_index = peak_points_size / 2 + random_int(8) - 4;
 
-  uint8_t dark_points_size = peak_index + edge_points_size;
-  uint8_t light_points_size =
-      peak_points_size - peak_index + edge_points_size + 1;
+  uint8_t dark_peak_points_size = peak_index;
+  uint8_t dark_base_points_size = with_base ? dark_peak_points_size / 2 : 0;
+  uint8_t dark_points_size =
+      dark_peak_points_size + edge_points_size + dark_base_points_size;
   struct Point *dark_points = malloc(sizeof(struct Point) * dark_points_size);
+
+  uint8_t light_peak_points_size = peak_points_size - peak_index;
+  uint8_t light_base_points_size = with_base ? light_peak_points_size / 2 : 0;
+  uint8_t light_points_size =
+      light_peak_points_size + edge_points_size + light_base_points_size + 1;
   struct Point *light_points = malloc(sizeof(struct Point) * light_points_size);
 
-  int16_t span_height_up = height / (peak_index - 1);
-  int16_t span_height_down = height / (peak_points_size - peak_index + 1);
-  int16_t x_span = width / peak_points_size;
+  int16_t span_height_up =
+      (height + y_base_mid - y_base_right) / (dark_peak_points_size - 1);
+  int16_t span_height_down =
+      (height + y_base_mid - y_base_left) / (light_peak_points_size + 1);
+  int16_t span_x = width / peak_points_size;
+  int16_t x_rand = width / 100;
+  int16_t y_rand = height / 100;
 
-  int16_t x = x_center - width / 2;
-  int16_t y = _y;
+  uint8_t dark_index = 0;
+  uint8_t light_index = 0;
 
+  int16_t x;
+  int16_t y;
+
+  // Generate left side (darker) of mountain
+  // from the bottom left to the center peak.
   x = x_center - width / 2;
-  y = _y;
-  dark_points[0] = (struct Point){x, _y};
-  for (i = 1; i < peak_index; i++) {
-    x += x_span;
+  y = y_base_left;
+  dark_points[dark_index++] = (struct Point){x, y};
+  for (i = 1; i < dark_peak_points_size; i++) {
+    x += span_x;
     y -= span_height_up;
-    dark_points[i] =
-        (struct Point){x + random_int(32) - 64, y + random_int(32) - 64};
+    dark_points[dark_index++] =
+        (struct Point){x + random_int_b(x_rand), y + random_int_b(y_rand)};
   }
 
-  light_points[0] = dark_points[peak_index - 1];
-  int16_t span_height_edge = (_y - y) / edge_points_size;
+  // Generate edge of mountain for left and right side.
+  // from the center peak to the center bottom.
+  light_points[light_index++] = dark_points[dark_index - 1];
+  int16_t span_height_edge = (y_base_mid - y) / edge_points_size;
+  int16_t span_x_edge = random_int(x_rand * 2);
   for (i = 0; i < edge_points_size; i++) {
+    x += span_x_edge;
     y += span_height_edge;
-    struct Point point = {x + random_int(64) - 128, y + random_int(32)};
-    dark_points[peak_index + i] = point;
-    light_points[1 + i] = point;
+    int16_t rand = x_rand * 2 + (i - edge_points_size / 2);
+    struct Point point = {x + (rand > 0 ? random_int_b(rand) : 0),
+                          y + (rand > 0 ? random_int_b(rand) : 0)};
+    dark_points[dark_index++] = point;
+    light_points[light_index++] = point;
   }
-  dark_points[dark_points_size - 1].y = _y;
-  light_points[edge_points_size].y = _y;
+  dark_points[dark_index - 1].y = y_base_mid;
+  light_points[light_index - 1].y = y_base_mid;
 
+  // Generate base of the left side
+  // from the bottom center to the bottom left.
+  if (dark_base_points_size > 0) {
+    int x_dark_base = x;
+    int span_left_base = (y_base_left - y_base_mid) / dark_base_points_size;
+    y = y_base_mid;
+    for (i = 0; i < dark_base_points_size; i++) {
+      x_dark_base -= span_x * 2;
+      y += span_left_base;
+      struct Point point = {x_dark_base + +random_int_b(x_rand * 2),
+                            y + random_int_b(y_rand * 8)};
+      dark_points[dark_index++] = point;
+    }
+  }
+
+  // Generate base of the right side
+  // from the bottom center to the bottom right.
+  if (light_base_points_size > 0) {
+    int x_light_base = x;
+    int span_right_base = (y_base_right - y_base_mid) / light_base_points_size;
+    y = y_base_mid;
+    for (i = 0; i < light_base_points_size; i++) {
+      x_light_base += span_x * 2;
+      y += span_right_base;
+      struct Point point = {x_light_base + random_int_b(x_rand * 2),
+                            y + random_int_b(y_rand * 2) * 8};
+      light_points[light_index++] = point;
+    }
+  }
+
+  // Generate right side (lighter) of mountain
+  // from the bottom right to the center peak.
   x = x_center + width / 2;
-  y = _y;
-  light_points[edge_points_size + 1] = (struct Point){x, _y};
-  for (i = edge_points_size + 2; i < light_points_size; i++) {
-    x -= x_span;
+  y = y_base_right;
+  light_points[light_index++] = (struct Point){x, y};
+  for (i = 1; i < light_peak_points_size; i++) {
+    x -= span_x;
     y -= span_height_down;
-    light_points[i] =
-        (struct Point){x + random_int(32) - 64, y + random_int(32) - 64};
+    light_points[light_index++] =
+        (struct Point){x + random_int_b(x_rand), y + random_int_b(y_rand)};
   }
 
   _polyfill_mountain(image, dark_points, dark_points_size, 96u);
